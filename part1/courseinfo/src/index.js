165ebbb6ca7ef776584e25f413f988cc2a753a1a
Battulga Enkhbayar <tugushka11@gmail.com>
2020-05-23 15:03:21 +0300

    exercise 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -9,12 +9,20 @@ const Header = (props) => {
   );
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
     </div>
   );
 }
@@ -39,9 +47,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
       <Total totalExercises={exercises1 + exercises2 + exercises3}/>
     </div>
   )

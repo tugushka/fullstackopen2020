751be6d32ad5e8da7fb859df96205bca50344484
Battulga Enkhbayar <tugushka11@gmail.com>
2020-05-23 20:11:14 +0300

    exercise 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Anecdote = ({anecdote, vote}) => {
+  return (
+    <div>
+      <p>{anecdote}</p>
+      <p>has {vote} {vote>1 ? 'votes' : 'vote'}</p>
+    </div>
+  )
+}
+
+const AnecdoteWithMostVotes = ({anecdotes, votes}) => {
+  let cur_max = votes[0], id_max = 0;
+  for(let i = 1 ; i < votes.length ; i++) {
+    if( votes[i] > cur_max ) {
+      cur_max = votes[i];
+      id_max = i;
+    }
+  }
+
+  return (
+    <Anecdote anecdote={anecdotes[id_max]} vote={votes[id_max]} />
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
@@ -13,12 +36,14 @@ const App = (props) => {
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]}</p>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={anecdotes[selected]} vote={votes[selected]} />
       <div>
         <button onClick={handleVoteClick}>vote</button>
         <button onClick={() => {setSelected( Math.floor(Math.random()*anecdotes.length) )}}>next anecdote</button>
       </div>
+
+      <AnecdoteWithMostVotes anecdotes={anecdotes} votes={votes}/>
     </div>
     )
 }

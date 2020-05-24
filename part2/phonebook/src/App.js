5b88cbd4f1d60ddca07660d84dbb86c0e903d5d3
Battulga Enkhbayar <45190202+tugushka@users.noreply.github.com>
2020-05-24 22:19:24 +0300

    Removed the old commented template

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -23,68 +23,6 @@ const App = () => {
       <Persons persons={persons}/>
     </div>
   )
-
-  /*
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filteredPersons, setNewFilteredPersons] = useState([]);
-
-  const handleNameChange = (event) => {
-    setNewName( event.target.value );
-  }
-
-  const handleNameSubmit = (event) => {
-    event.preventDefault();
-    console.log(persons);
-    if( persons.find( person => person.name === newName ) ) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat({name: newName, number: newNumber}));
-      // console.log('Saved', newName);
-    }
-  }
-
-  const handleNumberChange = (event) => {
-    // console.log(event.target.value);
-    setNewNumber( event.target.value );
-  }
-
-  const handleFilterChange = (event) => {
-    setNewFilteredPersons( 
-      persons.filter( person => 
-        person.name.toLowerCase().includes(
-          event.target.value.toLowerCase()
-        )
-      )
-    )
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <div>
-        <p>filter shown with <input onChange={handleFilterChange} /></p>
-        {
-          filteredPersons.map( person => (
-            <p key={person.name}>{person.name} {person.number}</p>
-          ))
-        }
-      </div>
-
-      <form onSubmit={handleNameSubmit}>
-        <div>name: <input onChange={handleNameChange} /></div>
-        <div>number: <input onChange={handleNumberChange} /></div>
-        <div><button type="submit">add</button></div>
-      </form>
-      <h2>Numbers</h2>
-      {
-        persons.map( person => (
-        <p key={person.name}>{person.name} {person.number}</p>
-        ))
-      }
-    </div>
-  )
-  */
 }
 
 export default App

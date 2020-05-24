e0c3182bc1e226c3119c30ef94d33909b1a2c8c0
Battulga Enkhbayar <tugushka11@gmail.com>
2020-05-24 22:17:11 +0300

    exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -7,6 +10,21 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter persons={persons} />
+
+      <h3>Add a new</h3>
+      <PersonForm persons={persons} setPersons={setPersons}/>
+
+      <h3>Numbers</h3>
+      <Persons persons={persons}/>
+    </div>
+  )
+
+  /*
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filteredPersons, setNewFilteredPersons] = useState([]);
@@ -66,6 +84,7 @@ const App = () => {
       }
     </div>
   )
+  */
 }
 
 export default App
---
+++ part2/phonebook/src/components/PersonForm.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+const PersonForm = ({persons, setPersons}) => {
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  
+  const handleNameChange = (event) => {
+    setNewName( event.target.value );
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber( event.target.value );
+  }
+
+  const handleNameSubmit = (event) => {
+    event.preventDefault();
+    console.log(persons);
+    if( persons.find( person => person.name === newName ) ) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({name: newName, number: newNumber}));
+    }
+  }
+
+  return (
+    <form onSubmit={handleNameSubmit}>
+      <div>name: <input onChange={handleNameChange} /></div>
+      <div>number: <input onChange={handleNumberChange} /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
+}
+
+export default PersonForm

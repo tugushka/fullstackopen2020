860fc601f6d2b83076c086c9bcf8477f67376b7b
Battulga Enkhbayar <tugushka11@gmail.com>
2020-05-24 18:07:14 +0300

    exercise 2.1

---
+++ part2/courseinfo/README.md
@@ -0,0 +1,2 @@
+Course Information
+Exercise 1.3-5
---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+}
+
+const Part = ({part}) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map( part => 
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  );
+}
+
+const Total = (props) => {
+  let totalExercises = 0;
+  props.parts.forEach(part => {
+    totalExercises += part.exercises;
+  });
+  return (
+    <div>
+      <h4>Number of exercises {totalExercises}</h4>
+    </div>
+  );
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

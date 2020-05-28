635a14eea863c21aa680fc7ae6fc22f49816daea
Battulga Enkhbayar <tugushka11@gmail.com>
2020-05-29 00:43:34 +0300

    exercise 3.2

--- part3/phonebook_backend/src/index.js
+++ part3/phonebook_backend/src/index.js
@@ -28,6 +28,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${(new Date()).toUTCString()}</p>
+  `)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })

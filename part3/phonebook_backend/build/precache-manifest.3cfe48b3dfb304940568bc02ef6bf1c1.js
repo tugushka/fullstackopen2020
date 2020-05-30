3a26f2cfd8a288971a40e25bd540448334decfe6
Battulga Enkhbayar <tugushka11@gmail.com>
2020-05-30 21:55:17 +0300

    Removing /build from gitignore in order to deploy on heroku

---
+++ part3/phonebook_backend/build/precache-manifest.3cfe48b3dfb304940568bc02ef6bf1c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4b433ab101d23dc724a8169ae7866e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "662f56e7d4b775773209",
+    "url": "/static/css/main.8e0cb423.chunk.css"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662f56e7d4b775773209",
+    "url": "/static/js/main.a90bc21e.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);

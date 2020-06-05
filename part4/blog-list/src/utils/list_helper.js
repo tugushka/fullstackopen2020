8121b86d498640d96158bf60635e5a2e8b89981c
Battulga Enkhbayar <tugushka11@gmail.com>
2020-06-05 23:11:50 +0300

    Exercise 4.5

--- part4/blog-list/src/utils/list_helper.js
+++ part4/blog-list/src/utils/list_helper.js
@@ -6,4 +6,9 @@ const totalLikes = (blogs) => {
   return blogs.reduce( (sum, blog) => sum+blog.likes, 0 );
 }
 
-module.exports = {dummy, totalLikes};
+const favoriteBlog = (blogs) => {
+  const mostLikes = blogs.reduce( (cur_max, blog) => Math.max(cur_max, blog.likes), 0 );
+  return blogs.find( blog => blog.likes === mostLikes );
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog};
